bc8ac895703a273147504b5dbac5c04db363ed80
Karamellen123 <ellen.s.davidsson@gmail.com>
2024-02-23 13:27:42 +0100

    Changed from dictionary into classes fixed the code after that added mark task as completed

--- Program.cs
+++ Program.cs
@@ -1,18 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-List<string> TaskName = new List<string>();
-List<int> TaskDate = new List<int>();
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+//List<string> TaskName = new List<string>();
+//List<int> TaskDate = new List<int>();
 List<bool> isTaskCompleted = new List<bool>();
 
-Dictionary<string, int> TasksUndone = new Dictionary<string, int>();
-Dictionary<string, int> TasksCompleted = new Dictionary<string, int>();
+List<TaskUndone> Uncompleted = new List<TaskUndone>();
+List<TaskCompleted> CompletedList = new List<TaskCompleted>();
+
+
+//Dictionary<string, int> TasksUndone = new Dictionary<string, int>();
+//Dictionary<string, int> TasksCompleted = new Dictionary<string, int>();
 
 //Adding random numbers so it won't be empty the first time (mainly used for debugging)
-TasksUndone.Add("-.-", 9);
-TasksUndone.Add("Coding", 2);
-TasksUndone.Add("Food", 5);
-TasksUndone.Add("Pommes", 3);
-TasksUndone.Add("Chocolate", 8);
-TasksUndone.Add("Paint", 4);
+Uncompleted.Add(new TaskUndone("-.-", 9));
+Uncompleted.Add(new TaskUndone("Coding", 2));
+Uncompleted.Add(new TaskUndone("Food", 5));
+Uncompleted.Add(new TaskUndone("Pommes", 3));
+Uncompleted.Add(new TaskUndone("Chocolate", 8));
+Uncompleted.Add(new TaskUndone("Paint", 4));
 
 //LoadTasksFromFile("tasks.txt"); // Load tasks from file when the program starts
 MainMenu();
@@ -26,8 +33,8 @@ void AddTaskToList()
 
     Console.WriteLine("Enter a deadline: ");
     date = Convert.ToInt32(Console.ReadLine());
-    
-    TasksUndone.Add(answer, date);
+
+    Uncompleted.Add(new TaskUndone(answer, date));
     // return to Main Menu
     MainMenu();
 
@@ -41,22 +48,26 @@ void ChangeDate()
     Console.WriteLine("Which task would you like to change the date on? ");
 
     //Lists the tasks so user can choose which task they want to change
-    for (int i = 0; i < TasksUndone.Count; i++)
+    for (int i = 0; i < Uncompleted.Count; i++)
     {
-        Console.WriteLine(i + " " + TasksUndone.Keys.ElementAt(i) + ", current date: " + TasksUndone.Values.ElementAt(i));
+        Console.WriteLine(i + " " + Uncompleted[i].Name + ", current date: " + Uncompleted[i].Deadline);
     }
 
     selected = Convert.ToInt32(Console.ReadLine());
 
     Console.WriteLine("What would you like to change it to? ");
     newDate = Convert.ToInt32(Console.ReadLine());
-    
+
     //Change selected valuse to new date
-    string selectedTask = TasksUndone.Keys.ElementAt(selected);
-    TasksUndone[selectedTask] = newDate;
+    Uncompleted[selected].Deadline = newDate;
+
+    /*
+    int selectedTask = Uncompleted[selected].Deadline;
+    Uncompleted[selectedTask] = newDate;
+    */
 
     //Displays the new date to user
-    Console.WriteLine("The Task: " + TasksUndone.Keys.ElementAt(selected) + " have changed it's deadline to: " + TasksUndone.Values.ElementAt(selected));
+    Console.WriteLine("The Task: " + Uncompleted[selected].Name + " have changed it's deadline to: " + Uncompleted[selected].Deadline);
 
     // return to Main menu
     MainMenu();
@@ -68,22 +79,23 @@ void ShowTaskList()
     Console.WriteLine(" Your todo list:");
     
     // Displays the task list
-    for (int i = 0; i < TasksUndone.Count; i++)
+    for (int i = 0; i < Uncompleted.Count; i++)
     {
         //Console.WriteLine(i + ") Task name:" + TaskName[i] + ", current date: " + TaskDate[i] + ", status: " + isTaskCompleted[i]);
-        Console.WriteLine(i + ") Task name: " + TasksUndone.Keys.ElementAt(i) + ", current date: " + TasksUndone.Values.ElementAt(i));
+        Console.WriteLine(i + ") Task name: " + Uncompleted[i].Name + ", current date: " + Uncompleted[i].Deadline);
     }
     Console.ReadKey();
     // return to main menu
     MainMenu();
 
 }
-void sortListbyName()
+void ViewUncompletedListbyName()
 {
-    var sortedKeys = TasksUndone.Keys.OrderBy(key => key).ToList(); // Fick hjälp av chat GPT med den här raden, fick inte ihop det, ska kolla LINQ igen
-    foreach (var key in sortedKeys)
+    //var sortedValue = TasksUndone.Values.OrderBy(value => value).ToList();
+    var sortedTasks = Uncompleted.OrderBy(task => task.Name).ToList(); 
+    foreach (var task in sortedTasks)
     {
-        Console.WriteLine("Task: " + key + ", Deadline: " + TasksUndone[key]);
+        Console.WriteLine("Task: " + task.Name + ", Deadline: " + task.Deadline);
     }
     Console.ReadKey();
     MainMenu();
@@ -91,10 +103,10 @@ void sortListbyName()
 
 void SortListByDate()
 {
-    var sortedKeys = TasksUndone.Keys.OrderBy(key => TasksUndone[key]).ToList();
-    foreach (var key in sortedKeys)
+    var sortedTasks = Uncompleted.OrderBy(task => task.Deadline).ToList();
+    foreach (var task in sortedTasks)
     {
-        Console.WriteLine("Task: " + key + ", Deadline: " + TasksUndone[key]);
+        Console.WriteLine("Task: " + task.Name + ", Deadline: " + task.Deadline);
     }
     Console.ReadKey();
     MainMenu();
@@ -104,20 +116,72 @@ void SortListByDate()
 void MainMenu()
 {
     Console.Clear();
-    Console.WriteLine("Welcome to Todo List! \nYou have " + TasksUndone.Count + " tasks to do and " + TasksCompleted.Count + " completed. \nPick an option: ");
+    Console.WriteLine("Welcome to Todo List! \nYou have " + Uncompleted.Count + " tasks to do and " + Uncompleted.Count + " completed. \nPick an option: ");
     Console.WriteLine("1) View todo list");
     Console.WriteLine("2) Add Task to your todo list");
-    Console.WriteLine("3) change date");
+    Console.WriteLine("3) change date \n4) Mark Task as done");
 
     string answer = "";
     answer = Console.ReadLine();
 
-    if (answer == "1") { sortListbyName(); }
+    if (answer == "1") { ViewUncompletedListbyName(); }
     else if (answer == "2") { AddTaskToList(); }
     else if (answer == "3") { ChangeDate(); }
+    else if(answer == "4") { TaskIsCompleted(); }
    // else if(answer == "4") { sortListbyName(); }
     else { Console.WriteLine("temp"); }
 
+}
+
+void TaskIsCompleted()
+{
+    var sortedUncompletedTasks = Uncompleted.OrderBy(task => task.Deadline).ToList();
+    for (int i = 0; i < sortedUncompletedTasks.Count; i++)
+    {
+        Console.WriteLine(i + ") Task: " + sortedUncompletedTasks[i].Name + ", Deadline: " + sortedUncompletedTasks[i].Deadline);
+    }
+    Console.WriteLine("Enter the number of the completed task: ");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    CompletedList.Add(new TaskCompleted(sortedUncompletedTasks[answer].Name, sortedUncompletedTasks[answer].Deadline));
+    Uncompleted.Remove(sortedUncompletedTasks[answer]); // Remove directly from sorted list
+
+    var sortedCompletedTasks = CompletedList.OrderBy(task => task.Deadline).ToList();
+    Console.WriteLine("____________________________________________________________________");
+    Console.WriteLine("Completed Tasks");
+    for (int i = 0; i < sortedCompletedTasks.Count; i++)
+    {
+        Console.WriteLine(i + ") Task: " + sortedCompletedTasks[i].Name + ", Deadline: " + sortedCompletedTasks[i].Deadline);
+    }
+
+    Console.WriteLine("____________________________________________________________________");
+    Console.WriteLine("Incompleted Tasks: ");
+    SortListByDate();
+    Console.ReadKey();
+
+}
+
+class TaskUndone 
+
+{
+    public TaskUndone(string name, int deadline)
+    {
+        Name = name;
+        Deadline = deadline;
+    }
+    public string Name { get; set; }
+    public int Deadline { get; set; }
+
+}
+
+class TaskCompleted
+{
+    public TaskCompleted(string name, int deadline)
+    {
+        Name = name;
+        Deadline = deadline;
+    }
+    public string Name { get; set; }
+    public int Deadline { get; set; }
 }
 
 /*
@@ -142,6 +206,7 @@ static void SaveTasksToFile(string filename, Dictionary<string, int> tasksUndone
     {
         foreach (var task in tasksUndone)
         {
+
             writer.WriteLine(task.Key + "," + task.Value);
         }
     }
